5e5a01265a5587822b85aede4c9f9bc6995bb075
Matt Brennan <mattyb1000@gmail.com>
2016-06-13 16:51:01 +0100

    query last week for fresher search terms, barf if not enough data

--- src/get-search-terms.js
+++ src/get-search-terms.js
@@ -7,7 +7,7 @@ export default async function(options) {
 	let o;
 	const results = await webmasters.searchAnalytics.query('www.ft.com', {
 		endDate: moment().format('YYYY-MM-DD'),
-		startDate: moment().subtract(1, 'month').format('YYYY-MM-DD'),
+		startDate: moment().subtract(1, 'week').format('YYYY-MM-DD'),
 		dimensions: ['query'],
 		dimensionFilterGroups: [{
 			groupType: 'and',
@@ -26,6 +26,10 @@ export default async function(options) {
 		}],
 		...options.query,
 	}, accessToken, clientId);
+
+	if(!results[1].rows) {
+		throw new Error('Not enough data from Google search analytics');
+	}
 
 	return results[1].rows;
 }

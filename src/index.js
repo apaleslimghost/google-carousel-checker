acfc793b3f19cf6a3236eac60cddf242f8381e95
Matt Brennan <mattyb1000@gmail.com>
2016-06-13 16:52:41 +0100

    just output the data

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import getCarouselPositions from './get-carousel-positions';
 import getSearchTerms from './get-search-terms';
-import groupBy from 'lodash.groupby';
-import mapValues from 'lodash.mapvalues';
-import gaussian from '@quarterto/gaussian';
 
 export default async function(options) {
 	const searchTerms = await getSearchTerms(options);
 	return await Promise.all(
 		searchTerms.map(async function(term) {
-			const links = mapValues(
-				groupBy(
-					await getCarouselPositions(term.keys[0]), 'publisher'
-				),
-				results => gaussian(results.map(({position}) => position))
-			);
+			const links = await getCarouselPositions(term.keys[0]);
 			return {term, links};
 		})
 	);

040f409e0bf82569e4c5e71bea3ad93144ca9537
Matt Brennan <mattyb1000@gmail.com>
2016-06-13 17:09:24 +0100

    packaging

--- src/access-token.js
+++ src/access-token.js
@@ -3,7 +3,7 @@ import {oauth2} from 'google-api-client';
 let accessToken = process.env.ACCESS_TOKEN;
 
 export default async function({refreshToken, clientId, clientSecret}) {
-	if(!accessToken || !await oauth2.isTokenValid(accessToken)){
+	if(!accessToken || !await oauth2.isTokenValid(accessToken)) {
 		accessToken = await oauth2.refreshToken(refreshToken, clientId, clientSecret);
 	}
 
